e488ab02e751d2e8f444e963300a6ff0f9d15b09
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-22 20:25:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,8 @@
+class TypeMismatchError < TypeError; end
 class Transfer
   # your code here
   def initialize(from, to, amount)
+    raise TypeMismatchError, "Sender object class #{from.class} received, BankAccount expected" if !from.is_a?(BankAccount)
     @sender = from
     @receiver = to
     @amount = amount
@@ -9,5 +11,11 @@ class Transfer
 
   def valid?
     @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if (@status == pending)
+      
+    end
   end
 end
