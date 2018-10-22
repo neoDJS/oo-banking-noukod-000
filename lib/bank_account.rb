6d6f51517ff6fe2b018c12f1ff769cc6194d7d01
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-22 20:39:03 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
+class AccessError < KeyError; end
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name="")
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def name=(name="")
+    raise AccessError, "You can't change the name of the Bank Account"
+  end
+
+  def deposit(money=0)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+    @balance = 0
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
+class TypeMismatchError < TypeError; end
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :status, :amount
+  def initialize(from, to, amount)
+    raise TypeMismatchError, "Sender object class is #{from.class}, BankAccount expected" if !from.is_a?(BankAccount)
+    raise TypeMismatchError, "Receiver object class is #{to.class}, BankAccount expected" if !to.is_a?(BankAccount)
+    @sender = from
+    @receiver = to
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    (@sender.valid? && @sender.balance > @amount) && (@receiver.valid?) && (status == "pending")
+  end
+
+  def execute_transaction
+    if self.valid?
+      @sender.deposit(-1*@amount)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if(@status == "complete")
+      @receiver.deposit(-1*@amount) if @receiver.balance > @amount
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
+  end
 end
